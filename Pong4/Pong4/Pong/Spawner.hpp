be93f0a0ac890baa17143a83b4e42766f6aced2b
phoenixstaley121@hotmail.com <phoenixstaley121@hotmail.com>
2023-04-24 20:30:12 -0700

    Add getters and setters

--- Pong4/Pong4/Pong/Spawner.hpp
+++ Pong4/Pong4/Pong/Spawner.hpp
@@ -14,6 +14,14 @@ class Spawner {
 		Spawner(int currHeight, int timeToObstacle, const int unitSize, sf::Texture* textures, std::vector<Obstacle*>* objectVect, double* currSpeed);
 		~Spawner() {}
 
+		int getHeight(void) { return this->currHeight; }
+		void setHeight(int newHeight) { this->currHeight = newHeight; }
+		int getTimeToObstacle(void) { return this->currHeight; }
+		void setTimeToObstacle(int newTime) { this->timeToObstacle = newTime; }
+		int getSpeed(void) { return *(this->currSpeed); }
+		void setSpeed(int newSpeed) { *(this->currSpeed) = newSpeed; }
+		std::vector<Obstacle*>& getObjVect(void) { return *(this->objectVect); }
+
 		void cleanOutObstacles(void);
 
 		void spawnNewGround(void);
